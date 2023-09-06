840753d500b6a8b6d187ab7568f43b766ab0ccef
ShellFansJason <jason.huang@shell.fans>
2023-09-06 11:13:15 +0800

    change_setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,6 +87,13 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+# SMTP mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True  # 使用TLS加密
+EMAIL_HOST_USER = 'hello@shell.fans'
+EMAIL_HOST_PASSWORD = 'T6u65lS@yU-SOA65'
 
 REGION = 'asia-east1'
 
