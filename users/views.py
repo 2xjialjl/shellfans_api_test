de10f746af48946ba39c01339c6eff1b010e0179
ShellFansJason <jason.huang@shell.fans>
2023-09-07 15:50:41 +0800

    change_send_login_email_url

--- users/views.py
+++ users/views.py
@@ -208,7 +208,7 @@ def send_login_email(request):
                 'message': 'Sending email successfully',
                 'data': {
                     'code': status.HTTP_200_OK,
-                    'data':login_verification_code
+                    'data123':login_verification_code
                 }
             }
             return Response(response_correct_data, status=status.HTTP_200_OK)
