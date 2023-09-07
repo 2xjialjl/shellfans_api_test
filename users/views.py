22ed26034de3e3b1f17a9e14e681645987dfdbb2
ShellFansJason <jason.huang@shell.fans>
2023-09-07 15:02:45 +0800

    login_email2

--- users/views.py
+++ users/views.py
@@ -194,7 +194,10 @@ def send_login_email(request):
         # 驗證碼存入cache中
         cache.set(email, {'code': login_verification_code, 'expiration': login_expiration_time}, 600)
         # 發送email
-        html_message = render_to_string('email_template.html', {'login_verification_code': login_verification_code})
+        email = request.data.get('email')
+        verification_code = str(random.randint(100000, 999999))
+        # 發送email
+        html_message = render_to_string('email_template.html', {'verification_code': verification_code})
         subject = 'shellfans 登入驗證信'
         from_email = 'hello@shell.fans'
         recipient_list = [email]
