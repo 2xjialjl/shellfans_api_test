ec1a1ce72967f0ec2f682c7e761e621800fce1f0
ShellFansJason <jason.huang@shell.fans>
2023-09-12 15:34:05 +0800

    edit email html

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 # users/models.py
 
 from django.db import models
-from django.utils import timezone
 class User(models.Model):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
