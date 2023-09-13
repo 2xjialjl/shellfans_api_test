2b49b6b79a5088949caf29cb8d472653d4ba3595
ShellFansJason <jason.huang@shell.fans>
2023-09-13 13:56:10 +0800

    add phone na

--- users/models.py
+++ users/models.py
@@ -2,11 +2,11 @@
 
 from django.db import models
 class User(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, null=True)
     name = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
     birthday = models.DateField()
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True)
     profile_picture = models.TextField(blank=True)
     level = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
