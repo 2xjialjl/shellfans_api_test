ced86c6dcefaf7b8ac68790c517f3b3e60bd4e6c
ShellFansJason <jason.huang@shell.fans>
2023-09-20 17:57:48 +0800

    edit get_user_info

--- users/views.py
+++ users/views.py
@@ -602,19 +602,9 @@ def get_user_info(request):
         }
         return Response(response_data, status=status.HTTP_401_UNAUTHORIZED)
     try:
-        if authorization_header.startswith('Bearer '):
-            token = authorization_header[len('Bearer '):]
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
-        else:
-            # 如果不以 "Bearer " 开头，处理错误情况
-            response_data = {
-                'result': False,
-                'message': 'Invalid authorization header',
-                'data': {
-                    'code': status.HTTP_400_BAD_REQUEST,
-                }
-            }
-            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+        _, token = authorization_header.split()
+        token = str(token).replace("Bearer ", '')
+        payload = jwt.decode(token, 'secret', algorithms=['HS256'])
     except jwt.ExpiredSignatureError:
         # Token過期
         response_data = {
