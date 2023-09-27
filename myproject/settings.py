518b6a5ab1493e0080fe75c5e43beddeee36cef2
ShellFansJason <jason.huang@shell.fans>
2023-09-27 15:49:17 +0800

    add REST_FRAMEWORK

--- myproject/settings.py
+++ myproject/settings.py
@@ -154,3 +154,11 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
