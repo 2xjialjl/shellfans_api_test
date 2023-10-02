df207b3c973615c4c465aed6d4853e8e13e2e4c0
ShellFansJason <jason.huang@shell.fans>
2023-10-02 16:42:18 +0800

    refresh_token

--- users/views.py
+++ users/views.py
@@ -697,9 +697,12 @@ def get_user_info(request):
 @authentication_classes([TokenAuthentication])
 def refresh_token(request):
     try:
+        old_token = request.auth
+        user_id = old_token.payload.get('user_id')
         # 使用 SimpleJWT 提供的方法来獲取 refresh token
-        refresh_token = RefreshToken(request.auth)
-        # 刷新 token
+        refresh_token = RefreshToken(old_token)
+        access_token = refresh_token.access_token
+        access_token['user_id'] = user_id
         new_access_token = str(refresh_token.access_token)
 
         response_data = {
