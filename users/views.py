d7d9abb5036da6dbe8f9242d204bfa052e67eba2
ShellFansJason <jason.huang@shell.fans>
2023-10-02 21:37:17 +0800

    refresh_token

--- users/views.py
+++ users/views.py
@@ -721,6 +721,10 @@ def refresh_token(request):
             'data': {
                 'code': status.HTTP_400_BAD_REQUEST,
                 'error': str(e),
+                'token':token,
+                'decoded_payload':decoded_payload,
+                'user':user,
+                'refresh_token':refresh_token
             }
         }
         return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
